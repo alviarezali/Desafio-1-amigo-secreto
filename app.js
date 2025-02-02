85b3fe01169ce26fa1b7d0750521a30529639731
Daniel Alviarez <alviarezali4@gmail.com>
2025-02-02 11:42:30 -0300

    sorteio aleatorio

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let entrada = document.getElementById("amigo");
 let listaDeAmigos = document.getElementById("listaAmigos");
-let resultado = document.getAnimations("resultado")
+let resultado = document.getElementById("resultado")
 let amigos = []
+
 function adicionarAmigo(){
   let valor = entrada.value.trim();
   if(valor===""){
-    alert("Não insira um nome válido, tente novamente.")
-  }
-  amigos.push(valor);
-  listaDeAmigos.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join(""); 
-  entrada.value = ""
-}
+      alert("Não insira um nome válido, tente novamente.")
+    };
+    amigos.push(valor);
+    listaDeAmigos.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join(""); 
+    entrada.value = ""
+    
+};
+
+function sortearAmigo(){
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    resultado.innerHTML = `<li>${amigos[indiceAleatorio]}</li>`.toUpperCase()
+
+};
+
