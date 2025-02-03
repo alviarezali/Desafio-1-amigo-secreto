a49b65416121404c4a4a9cbc78dfc21f52a9e433
Daniel Alviarez <alviarezali4@gmail.com>
2025-02-03 09:51:16 -0300

    otimizando o innerHTML e adicionando a função restartList

--- app.js
+++ app.js
@@ -3,21 +3,35 @@ let entrada = document.getElementById("amigo");
 let listaDeAmigos = document.getElementById("listaAmigos");
 let resultado = document.getElementById("resultado")
 let amigos = []
+let count = 0
 
 function adicionarAmigo(){
   let valor = entrada.value.trim();
   if(valor===""){
-      alert("Não insira um nome válido, tente novamente.")
-    };
-    amigos.push(valor);
-    listaDeAmigos.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join(""); 
-    entrada.value = ""
-    
+      alert("Por favor, insira um nome.");
+      return;
+    }else if (amigos.includes(valor)) {
+      alert("Esse nome já foi adicionado.");
+      return;
+  };
+  amigos.push(valor);
+  listaDeAmigos.innerHTML += `<li>${amigos[count]}</li>`
+  // listaDeAmigos.innerHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join(""); 
+  entrada.value = ""
+  count +=1;
 };
 
 function sortearAmigo(){
-    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
-    resultado.innerHTML = `<li>${amigos[indiceAleatorio]}</li>`.toUpperCase()
-
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  resultado.innerHTML = `<li>Seu amigo secreto é: ${amigos[indiceAleatorio]}</li>`.toUpperCase()
+  reiniciarLista();
 };
+
+function reiniciarLista() {
+  amigos = [];
+  count = 0;
+  listaDeAmigos.innerHTML = ""; // Limpia el contenido de la lista de amigos
+  // resultado.innerHTML = ""; // Limpia el resultado del sorteo
+
+}
 
